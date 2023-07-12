9122f34317b0b56e728663fd58e3072a1fb4840e
Kevin Lim <limkevinkuan@gmail.com>
2023-07-12 15:43:51 -0700

    Make device_init available to subdrivers

--- src/init.lua
+++ src/init.lua
@@ -16,7 +16,6 @@ local capabilities = require "st.capabilities"
 local ZigbeeDriver = require "st.zigbee"
 local defaults = require "st.zigbee.defaults"
 local clusters = require "st.zigbee.zcl.clusters"
-local configurationMap = require "configurations"
 local SimpleMetering = clusters.SimpleMetering
 local ElectricalMeasurement = clusters.ElectricalMeasurement
 local preferences = require "preferences"
@@ -40,37 +39,7 @@ local do_configure = function(self, device)
   end
 end
 
-local function component_to_endpoint(device, component_id)
-  local ep_num = component_id:match("switch(%d)")
-  return ep_num and tonumber(ep_num) or device.fingerprinted_endpoint_id
-end
-
-local function endpoint_to_component(device, ep)
-  local switch_comp = string.format("switch%d", ep)
-  if device.profile.components[switch_comp] ~= nil then
-    return switch_comp
-  else
-    return "main"
-  end
-end
-
-local device_init = function(self, device)
-  device:set_component_to_endpoint_fn(component_to_endpoint)
-  device:set_endpoint_to_component_fn(endpoint_to_component)
-
-  local configuration = configurationMap.get_device_configuration(device)
-  if configuration ~= nil then
-    for _, attribute in ipairs(configuration) do
-      device:add_configured_attribute(attribute)
-      device:add_monitored_attribute(attribute)
-    end
-  end
-
-  local ias_zone_config_method = configurationMap.get_ias_zone_config_method(device)
-  if ias_zone_config_method ~= nil then
-    device:set_ias_zone_config_method(ias_zone_config_method)
-  end
-end
+local device_init = require "device_init"
 
 local zigbee_switch_driver_template = {
   supported_capabilities = {
